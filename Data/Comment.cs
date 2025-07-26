99afc15ed4c13ac2604fe85b49032c9e5568207a
panosrodo <brandonroots@hotmail.gr>
2025-07-27 02:29:01 +0300

    feat: add Comment entity

---
+++ Data/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagerApp.Data
+{
+    public class Comment : BaseEntity
+    {
+        public string Text { get; set; } = null!;
+
+        public int UserId { get; set; }
+        public int TaskId { get; set; }
+
+        // Navigation properties
+        public User? User { get; set; }
+        public Task? Task { get; set; }
+    }
+}
