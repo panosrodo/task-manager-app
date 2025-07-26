e4bd0151a44ffd086be77ee23e6d811934cb79da
panosrodo <brandonroots@hotmail.gr>
2025-07-27 02:28:34 +0300

    feat: add Tag entity

---
+++ Data/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagerApp.Data
+{
+    public class Tag : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+
+        // Navigation property
+        public ICollection<TaskTag>? TaskTags { get; set; }
+    }
+}
