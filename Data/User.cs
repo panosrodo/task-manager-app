a1651bca5d12a8c62a350e65aa37445d7510d6a1
panosrodo <brandonroots@hotmail.gr>
2025-07-27 02:27:24 +0300

    feat: add User entity

---
+++ Data/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagerApp.Core.Enums;
+
+
+namespace TaskManagerApp.Data
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string PasswordHash { get; set; } = null!;
+
+        [Required]
+        public UserRole Role { get; set; } = UserRole.User;
+
+        // Navigation properties
+        public ICollection<Project>? OwnedProjects { get; set; }
+        public ICollection<Task>? AssignedTasks { get; set; }
+        public ICollection<Comment>? Comments { get; set; }
+    }
+}
