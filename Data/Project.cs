0ecfd694731038e61267dacaa4ee6299dc37af98
panosrodo <brandonroots@hotmail.gr>
2025-07-27 02:27:50 +0300

    feat: add Project entity

---
+++ Data/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagerApp.Data
+{
+    public class Project : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public int OwnerId { get; set; }
+
+        // Navigation properties
+        public User? Owner { get; set; }
+        public ICollection<Task>? Tasks { get; set; }
+    }
+}
