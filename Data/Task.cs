c1bbc2d8bc1864998fa180016d45b10ab5eb178d
panosrodo <brandonroots@hotmail.gr>
2025-07-27 02:28:16 +0300

    feat: add Task entity

---
+++ Data/Task.cs
@@ -0,0 +1,24 @@
+﻿using TaskManagerApp.Core.Enums;
+using TaskStatus = TaskManagerApp.Core.Enums.TaskStatus;
+
+namespace TaskManagerApp.Data
+{
+    public class Task : BaseEntity
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime DueDate { get; set; }
+
+        public TaskPriority Priority { get; set; }
+        public TaskStatus Status { get; set; }
+
+        public int ProjectId { get; set; }
+        public int AssigneeId { get; set; }
+
+        // Navigation properties
+        public Project? Project { get; set; }
+        public User? Assignee { get; set; }
+        public ICollection<Comment>? Comments { get; set; }
+        public ICollection<TaskTag>? TaskTags { get; set; }
+    }
+}
