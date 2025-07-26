58323f46ac76dec1b8e4b51e93174e50ee5d6521
panosrodo <brandonroots@hotmail.gr>
2025-07-27 02:29:47 +0300

    feat: add TaskTag entity for many-to-many relationship between Task and Tag

---
+++ Data/TaskTag.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManagerApp.Data
+{
+    public class TaskTag
+    {
+        public int TaskId { get; set; }
+        public int TagId { get; set; }
+
+        // Navigation properties
+        public Task? Task { get; set; }
+        public Tag? Tag { get; set; }
+    }
+}
