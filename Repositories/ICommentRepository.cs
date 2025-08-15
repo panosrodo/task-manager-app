8675c78b1e3b3e0fb4f08cf8e95d75a09edbdbd8
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:23:38 +0300

    feat: add ICommentRepository interface

---
+++ Repositories/ICommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using TaskManagerApp.Data;
+
+namespace TaskManagerApp.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task<Comment?> GetCommentAsync(int id);
+        Task<Comment?> UpdateCommentAsync(int id, Comment comment);
+        Task<List<Comment>> GetAllCommentsFilteredPaginatedAsync(int pageNumber, int pageSize,
+            List<Expression<Func<Comment, bool>>> predicates);
+    }
+}
