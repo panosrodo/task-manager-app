150317a49f0b3ad6bdbf81c3326e5a4e842208ec
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:31:48 +0300

    feat: implement UnitOfWork to centralize repository access

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using TaskManagerApp.Data;
+
+namespace TaskManagerApp.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TaskManagerDbContext _context;
+
+        public ITagRepository TagRepository { get; }
+        public ITaskTagRepository TaskTagRepository { get; }
+        public ICommentRepository CommentRepository { get; }
+        public ITaskItemRepository TaskItemRepository { get; }
+        public IUserRepository UserRepository { get; }
+        public IProjectRepository ProjectRepository { get; }
+
+        public UnitOfWork(
+            TaskManagerDbContext context,
+            ITagRepository tagRepository,
+            ITaskTagRepository taskTagRepository,
+            ICommentRepository commentRepository,
+            ITaskItemRepository taskItemRepository,
+            IUserRepository userRepository,
+            IProjectRepository projectRepository)
+        {
+            _context = context;
+            TagRepository = tagRepository;
+            TaskTagRepository = taskTagRepository;
+            CommentRepository = commentRepository;
+            TaskItemRepository = taskItemRepository;
+            UserRepository = userRepository;
+            ProjectRepository = projectRepository;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
