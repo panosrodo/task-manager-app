0986eb38221ce53bfc4b2a1f1851301feaf02b3a
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:24:38 +0300

    feat: add IProjectRepository interface

---
+++ Repositories/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using TaskManagerApp.Data;
+
+namespace TaskManagerApp.Repositories
+{
+    public interface IProjectRepository
+    {
+        Task<Project?> GetProjectAsync(int projectId);
+        Task<Project?> UpdateProjectAsync(int id, Project project);
+        Task<List<Project>> GetAllProjectsFilteredPaginatedAsync(int pageNumber, int pageSize,
+            List<Expression<Func<Project, bool>>> predicates);
+    }
+}
