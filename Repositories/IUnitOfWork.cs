e0e49ab78f442fbcd7d6c1906909ad261fa881d7
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:31:29 +0300

    feat: add IUnitOfWork interface for central repository access

---
+++ Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagerApp.Repositories
+{
+    public interface IUnitOfWork
+    {
+        ITagRepository TagRepository { get; }
+        ITaskTagRepository TaskTagRepository { get; }
+        ICommentRepository CommentRepository { get; }
+        ITaskItemRepository TaskItemRepository { get; }
+        IUserRepository UserRepository { get; }
+        IProjectRepository ProjectRepository { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
