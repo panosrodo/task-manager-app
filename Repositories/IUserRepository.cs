233f1f01d5a5f1034786d625382cbf444acafb95
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:28:50 +0300

    feat: add IUserRepository interface

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using TaskManagerApp.Data;
+
+namespace TaskManagerApp.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserAsync(string username, string password);
+        Task<User?> UpdateUserAsync(int id, User user);
+        Task<User?> GetByUsernameAsync(string username);
+        Task<List<User>> GetAllUsersFilteredPaginatedAsync(int pageNumber, int pageSize,
+            List<Expression<Func<User, bool>>> predicates);
+    }
+}
