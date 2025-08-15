3bb121005c9d7dea8e23bdaba45c0ca76f76fc58
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:27:47 +0300

    feat: add ITaskTagRepository interface

---
+++ Repositories/ITaskTagRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManagerApp.Data;
+
+namespace TaskManagerApp.Repositories
+{
+    public interface ITaskTagRepository
+    {
+        Task<TaskTag?> GetTaskTagAsync(int taskItemId, int tagId);
+        Task AddTaskTagAsync(TaskTag taskTag);
+        Task RemoveTaskTagAsync(int taskItemId, int tagId);
+        Task<List<TaskTag>> GetTagsForTaskAsync(int taskItemId);
+        Task<List<TaskTag>> GetTasksForTagAsync(int tagId);
+    }
+}
