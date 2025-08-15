52e04661b9aa0fedac7063a78351aaca7ec6efe3
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:34:02 +0300

    chore: add DI registration extension for repositories and UnitOfWork

---
+++ Repositories/RepositoriesDIExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskManagerApp.Repositories
+{
+    public static class RepositoriesDIExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<ITaskTagRepository, TaskTagRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<ITaskItemRepository, TaskItemRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
