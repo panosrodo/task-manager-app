f2ad7060546c9aa97dca328aec0ea4fb432761d3
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:25:45 +0300

    feat: add ITagRepository interface

---
+++ Repositories/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using TaskManagerApp.Data;
+
+namespace TaskManagerApp.Repositories
+{
+    public interface ITagRepository
+    {
+        Task<Tag?> GetTagAsync(int id);
+        Task<Tag?> UpdateTagAsync(int id, Tag tag);
+        Task<List<Tag>> GetAllTagsFilteredPaginatedAsync(int pageNumber, int pageSize,
+            List<Expression<Func<Tag, bool>>> predicates);
+    }
+}
