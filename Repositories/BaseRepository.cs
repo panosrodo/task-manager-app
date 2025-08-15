27f8152fdf26d2b6ea3ff5edb24f6bde7f6671fb
panosrodo <brandonroots@hotmail.gr>
2025-08-15 18:22:49 +0300

    feat: add generic BaseRepository for CRUD operations

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerApp.Data;
+
+namespace TaskManagerApp.Repositories
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly TaskManagerDbContext context;
+        protected readonly DbSet<T> dbSet;
+
+        public BaseRepository(TaskManagerDbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public virtual async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await dbSet.AddRangeAsync(entities);
+            await context.SaveChangesAsync();
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+
+        public virtual async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await dbSet.FindAsync(id);
+            if (entity == null) return false;
+
+            dbSet.Remove(entity);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public virtual async Task<T?> GetAsync(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public virtual async Task<int> GetCountAsync()
+        {
+            return await dbSet.CountAsync();
+        }
+    }
+}
