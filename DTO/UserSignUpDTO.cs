1df50eb74d8b48d71a8d7315f672b111ec690298
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:21:31 +0300

    feat: add UserSignUpDTO

---
+++ DTO/UserSignUpDTO.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagerApp.Core.Enums;
+
+namespace TaskManagerApp.DTO
+{
+    public class UserSignUpDTO
+    {
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(100, ErrorMessage = "Email must not exceed 100 characters.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, " +
+            "one digit, and one special character")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Firstname must be between 2 and 50 characters.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Lastname must be between 2 and 50 characters.")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [EnumDataType(typeof(UserRole), ErrorMessage = "Invalid user role")]
+        public UserRole UserRole { get; set; }
+    }
+}
