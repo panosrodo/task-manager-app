a7a470570e8672629069a7ce3e224fa4df2e3752
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:18:25 +0300

    feat: add TagCreateDTO

---
+++ DTO/TagCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagerApp.Core.Enums;
+
+namespace TaskManagerApp.DTO
+{
+    public class TagCreateDTO
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string? Description { get; set; }
+    }
+}
