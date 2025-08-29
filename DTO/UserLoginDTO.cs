717675f5db9b1d2512e595f50d390f286a0da136
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:21:00 +0300

    feat: add UserLoginDTO

---
+++ DTO/UserLoginDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApp.DTO
+{
+    public class UserLoginDTO
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, " +
+            "one digit, and one special character")]
+        public string? Password { get; set; }
+
+        public bool KeepLoggedIn { get; set; }
+    }
+}
