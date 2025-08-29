b5de6c4c18a75f24a3d26c0eb5180499b68b0208
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:20:07 +0300

    feat: add TaskTagCreateDTO

---
+++ DTO/TaskTagCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApp.DTO
+{
+    public class TaskTagCreateDTO
+    {
+        [Required]
+        public int TaskItemId { get; set; }
+
+        [Required]
+        public int TagId { get; set; }
+    }
+}
