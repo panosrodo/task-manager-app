95451c38165267b07d1d925b2899b6833df71440
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:19:33 +0300

    feat: add TaskItemReadOnlyDTO

---
+++ DTO/TaskItemReadOnlyDTO.cs
@@ -0,0 +1,16 @@
+﻿using TaskManagerApp.Core.Enums;
+
+namespace TaskManagerApp.DTO
+{
+    public class TaskItemReadOnlyDTO
+    {
+        public int TaskItemId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public int Priority { get; set; }
+        public TaskItemStatus Status { get; set; }
+        public int ProjectId { get; set; }
+        public int AssigneeId { get; set; }
+    }
+}
