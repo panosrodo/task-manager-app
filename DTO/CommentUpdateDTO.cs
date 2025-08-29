c7445b2fd293faa4b8b6981c12f8fc83fe2db186
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:17:17 +0300

    feat: add CommentUpdateDTO

---
+++ DTO/CommentUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApp.DTO
+{
+    public class CommentUpdateDTO
+    {
+        [Required]
+        public int CommentId { get; set; }
+
+        [Required]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "The comment must be between 1 and 1000 characters long.")]
+        public string Text { get; set; } = string.Empty;
+    }
+}
