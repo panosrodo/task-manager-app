12d6c49c862cbf5b7127698efa12aa7c769973c0
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:18:59 +0300

    feat: add TagUpdateDTO

---
+++ DTO/TagUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApp.DTO
+{
+    public class TagUpdateDTO
+    {
+        [Required(ErrorMessage = "TagId is required.")]
+        public int TagId { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string? Description { get; set; }
+    }
+}
