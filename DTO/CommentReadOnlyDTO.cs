f7bfdd7c173eb82a61765e636267f05cdffbf43e
panosrodo <brandonroots@hotmail.gr>
2025-08-29 15:16:59 +0300

    feat: add CommentReadOnlyDTO

---
+++ DTO/CommentReadOnlyDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerApp.DTO
+{
+    public class CommentReadOnlyDTO
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public int TaskItemId { get; set; }
+        public int UserId { get; set; }
+    }
+}
